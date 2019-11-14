d276b1d1b62469680cac20a60cbe11d01d31e93c
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2019-11-14 10:55:14 +0100

    s

--- tema 3/tres en ralla/script1.js
+++ tema 3/tres en ralla/script1.js
@@ -3,7 +3,7 @@ import {entornoGrafico} from './script3.js'
 //console.log(cube(3)); 
 window.tresenralla=new tresEnRaya();
 //alert ('funciona')
-var entorno=new entornoGrafico(tresenralla); 
+var entorno = new entornoGrafico(tresenralla); 
 console.log(tresenralla.casillas);
 console.log(tresenralla.jugar(2,2));
     
--- tema 3/tres en ralla/script3.js
+++ tema 3/tres en ralla/script3.js
@@ -7,7 +7,15 @@ export class entornoGrafico{
         this.nuevatabla(3,3,juego);
 
     }; 
-
+    jugada(x,y){
+        var respuesta=tresenralla.jugar(x,y);
+        switch (respuesta){
+            case 1 :	alert("juego terminado,prueba ma suerte la prosima vez");break;
+            case 2 :	alert("casilla no valida, lla en uso");break;
+            case 3 :	alert("juego terminado,prueba ma suerte la prosima vez");break;
+            case 4 :    	alert("!!!!!porfin ganaste contra esta estupida maquina¡¡¡¡¡");break;
+        }
+    }
     
     nuevatabla(x,y){
     let tabla="<table>"
@@ -16,7 +24,7 @@ export class entornoGrafico{
     for (let i=0;i<x;i++){
         tabla+="<tr>";
         for (let z=0;z<y;z++){
-            tabla+="<td id=\"idenº\""+cont1+""+cont2+"><button onclick=\"tresenralla.jugar("+cont1+","+cont2+")\"></td>";
+            tabla+="<td id=\"idenº\""+cont1+""+cont2+"><button onclick=\"entorno.jugada("+cont1+","+cont2+")\"></td>";
             cont2++;
         }
         cont1++
