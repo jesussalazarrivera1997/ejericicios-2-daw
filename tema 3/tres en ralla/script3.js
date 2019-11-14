05d7dabd70208c83c7ec7def64c4d164c2f891fc
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2019-11-14 11:37:39 +0100

    Update script3.js

--- tema 3/tres en ralla/script3.js
+++ tema 3/tres en ralla/script3.js
@@ -16,6 +16,7 @@ export class entornoGrafico{
             case 4 :    alert("!!!!!porfin ganaste contra esta estupida maquina¡¡¡¡¡");break;
         }
         if (respuesta[1]!=10){
+            console.log(respuesta)
             document.getElementById("idenº"+respuesta[1][0]+""+respuesta[1][0]+"").style.backgroundColor="blue";
             document.getElementById("idenº"+respuesta[2][0]+""+respuesta[2][0]+"").style.backgroundColor="red";
         }
