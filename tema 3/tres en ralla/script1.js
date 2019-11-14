ed3010675feffb7db7694c18c202f3395a6ca1ac
jesussalazarrivera1997 <43956424+jesussalazarrivera1997@users.noreply.github.com>
2019-11-14 11:38:24 +0100

    Update script1.js

--- tema 3/tres en ralla/script1.js
+++ tema 3/tres en ralla/script1.js
@@ -3,7 +3,3 @@ import {entornoGrafico} from './script3.js'
 //console.log(cube(3)); 
 window.tresenralla=new tresEnRaya();
 //alert ('funciona')
-window.entorno = new entornoGrafico(tresenralla); 
-console.log(tresenralla.casillas);
-console.log(tresenralla.jugar(2,2));
-    
